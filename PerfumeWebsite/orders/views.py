018dcc2401050af362124ce744ebea7c97535005
oussama3422 <ossamarhayrhay2001@gmail.com>
2023-10-13 12:46:01 +0100

    upds

---
+++ PerfumeWebsite/orders/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render,redirect
+
+
+def  add_to_card(request):
+    return redirect('products')
+
+
--- PerfumeWebsite/perfumeWebsite/settings.py
+++ PerfumeWebsite/perfumeWebsite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'orders.apps.OrdersConfig',
     'products.apps.ProductsConfig',
     'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig',
